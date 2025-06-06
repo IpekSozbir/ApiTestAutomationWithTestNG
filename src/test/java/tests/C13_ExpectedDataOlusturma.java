6ea1309e72cb93b6d4e822402a6886d8c3e1fba0
IpekSozbir <ipekoren@gmail.com>
2025-06-06 16:35:07 +0300

    v12

---
+++ src/test/java/tests/C13_ExpectedDataOlusturma.java
@@ -0,0 +1,57 @@
+package tests;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C13_ExpectedDataOlusturma {
+
+    @Test
+    public void test() {
+            /*
+        https://jsonplaceholder.typicode.com/posts/22 url'ine
+        bir GET request yolladigimizda
+        donen response body’sinin asagida verilen ile ayni oldugunu test ediniz
+
+        Response body :
+        {
+        "userId":3,
+        "id":22,
+        "title":"dolor sint quo a velit explicabo quia nam",
+        "body":"eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+        }
+     */
+
+        // 1- EndPoint ve Request body olusturma
+        String url = "https://jsonplaceholder.typicode.com/posts/22";
+
+        // 2- Expected body olusturma
+        JSONObject expectedData = new JSONObject();
+        expectedData.put("userId",3);
+        expectedData.put("id",22);
+        expectedData.put("title","dolor sint quo a velit explicabo quia nam");
+        expectedData.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
+
+
+        // 3- Request gonderme ve Response kaydetme
+        Response response = given().when().get(url);
+
+        // 4- Assertion islemleri
+         /* Suana kadar yaptigimiz yontemle de testi yapabiliriz
+            Ancak framework'u gelistirmeye devam etmek icin artik JUnit Assert kullanacagiz
+          */
+
+        JsonPath responseJsonPath = response.jsonPath();
+        Assert.assertEquals(expectedData.get("id"),responseJsonPath.get("id"));
+        Assert.assertEquals(expectedData.get("title"),responseJsonPath.get("title"));
+        Assert.assertEquals(expectedData.get("body"),responseJsonPath.get("body"));
+        Assert.assertEquals(expectedData.get("userId"),responseJsonPath.get("userId"));
+
+
+    }
+
+}
